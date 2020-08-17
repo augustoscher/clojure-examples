c0c72b0cb85a2ef5b949e9c11dffd2cab2d5aac2
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-08-17 00:53:40 -0300

    Clojure Intro

--- tutorial/src/tutorial/core.clj
+++ tutorial/src/tutorial/core.clj
@@ -1,7 +1,7 @@
-(ns tutorial.core
-  (:gen-class))
+(ns tutorial.core ;; define namespace
+  (:gen-class)) ;; generate java class so we can run in jvm
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main ;; define function called main
+  "I don't do a whole lot ... yet." ;; description, not executed. Just a way to say what function does
+  [& args] ;; list of zero or more arguments
+  (println "Hello, World!")) ;; calls (verb) function println with arg "Hello World!"
