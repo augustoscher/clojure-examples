731808e7643741b5764b231f2951f99b78db95ac
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-07-16 23:01:38 -0300

    feat: operators in clojure

--- tutorial/src/tutorial/operators.clj
+++ tutorial/src/tutorial/operators.clj
@@ -10,13 +10,43 @@
   (println "Logical Operators")
   (println "Bitwise Operators"))
 
+;; ARITHMETIC
 (#(println "Sum" %1) (+ 1 2))
 (#(println "Sub" %1) (- 5 3))
 (#(println "Mult" %1) (* 3 6))
 (#(println "Div" %) (/ 3 2))
 
-(#(println "INC" %) (INC 6))
-(#(println "DEC" %) (DEC 6))
-(#(println "MAX" %) (MAX 3 6))
-(#(println "MIN" %) (MIN 3 6))
-(#(println "REM" %) (REM 6 3)) ;; remaining value when divide the first with de second
+;;(#(println "INC" %) (INC 6))
+;;(#(println "DEC" %) (DEC 6))
+;;(#(println "MAX" %) (MAX 3 6))
+;;(#(println "MIN" %) (MIN 3 6))
+;;(#(println "REM" %) (REM 6 3)) ;; remaining value when divide the first with de second
+
+;; RELATIONAL
+(defn -relational
+  "relational operators"
+  []
+  (= 2 2)
+  (< 3 6)
+  (> 6 3)
+  (<= 6 3)
+  ;;(NOT= 2 2)
+  )
+
+  ;; LOGICAL
+(defn -logical
+  "logical operators"
+  []
+  (AND true false) ;; false
+  (OR true false) ;; true
+  (NOT true)) ;; false
+
+  ;; BITWISE
+(defn -bitwise
+  "logical operators"
+  []
+  (BIT-AND 0101 1100) ;; 0100
+  (BIT-OR 0101 1100) ;; true
+  (BIT-XOR 0101 1100)
+  (BIT-NOT 0101 1100))
+  
