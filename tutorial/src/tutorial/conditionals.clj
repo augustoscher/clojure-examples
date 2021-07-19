a4daddb1af02146120613c4d3b8c4d03e0dc8a16
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-07-18 21:40:13 -0300

    feat: clojure if and ifdo conditionals

--- tutorial/src/tutorial/conditionals.clj
+++ tutorial/src/tutorial/conditionals.clj
@@ -1,7 +1,25 @@
 (ns tutorial.conditionals
   (:gen-class))
 
-(defn testConditionals
-  "Conditionals in clojure"
-  [& args]
-  (println "Hi")) 
+(defn testIf
+  "If conditional in clojure"
+  []
+  (println "\nIF")
+  (if (= 5 7)
+    (println "Equal")
+    (println "Not Equal")
+  ))
+(testIf)
+
+
+(defn testIfDo
+  "IfDo conditional in clojure"
+  []
+  (println "\nIFDO")
+  (if (= 5 3)
+    (do (println "Equal - First statement")
+        (println "Equal - Second statement"))
+    (do (println "Not Equal - First statement")
+        (println "Not Equal - Second statement"))
+  ))
+(testIfDo)
