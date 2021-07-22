20af28e012d75844ad24059808f87fd356c993cb
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-07-21 23:54:24 -0300

    feat: loop in clojure

--- tutorial/src/tutorial/loops.clj
+++ tutorial/src/tutorial/loops.clj
@@ -1,8 +1,13 @@
 (ns tutorial.loops
   (:gen-class))
 
-(defn test
+(defn testLoop
   "loop in clojure"
   []
-  (println "\nLOOP"))
-(test)
+  (println "\nLOOP")
+  (loop [value 0] ;; start a loop - assing 0 to value
+    (when (< value 10) ;; loop stop condition
+      (println value)
+      (recur (inc value))) ;; return to loop statement with incremented value
+  ))
+(testLoop) 
