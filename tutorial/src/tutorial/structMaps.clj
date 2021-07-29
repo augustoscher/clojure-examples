042b08d95630d2c065abbe9b09d1297210f5d900
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-07-29 10:09:34 -0300

    feat: structMaps in clojure

--- tutorial/src/tutorial/structMaps.clj
+++ tutorial/src/tutorial/structMaps.clj
@@ -4,6 +4,25 @@
 (defn testStructMaps
   "StructMaps in clojure"
   []
-  (println "\nStructMaps"))
+  (println "\nStructMaps")
+  (defstruct pet :PetType :PetName) ;; define the information that we want for out pet
+  (def myPet (struct pet "dog" "Marley")) ;; define struct myPet
+  (println myPet)
+
+  (def myPet2 (struct-map pet :PetName "Fred" :PetType "cat"))
+  (println myPet2)
+
+  (println (:PetName myPet))
+  (println (:PetName myPet2))
+
+  ;; structs or struct maps are immutable
+
+  (def myNewPet (assoc myPet :PetName "Max")) ;; define new struct based on existing one
+  (println myNewPet)
+  (println myPet)
+
+  (def myNewPet2 (assoc myNewPet :PetAge 10)) ;; define new attribute for struct
+  (println myNewPet2)
+  )
 
 (testStructMaps)
