e322e882d0377d511379c4e1dbc2737b160e252d
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-08-07 13:32:36 -0300

    feat: references in clojure

--- tutorial/src/tutorial/references.clj
+++ tutorial/src/tutorial/references.clj
@@ -4,5 +4,14 @@
 (defn testReferences
   []
   (println "\nReferences in clojure")
+  (def amount (ref 100))
+  (println @amount)
+
+  ;; dosync do things in transaction. If any error occours, all transactions are undone
+  (dosync (ref-set amount 110))
+  (println @amount)
+
+  (dosync (alter amount inc))
+  (println @amount)
 )
 (testReferences)
