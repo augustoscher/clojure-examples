9c9511160332d3f91589e2f9fdea7d95aa7c23c4
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-08-08 00:01:52 -0300

    feat: car dealership challenge

--- tutorial/src/tutorial/carDealerShip.clj
+++ tutorial/src/tutorial/carDealerShip.clj
@@ -2,11 +2,49 @@
   (:gen-class))
 
 ;; CODE CHALLENGE
+;; 3 types of cars: bmw 60000, ferrari 100000, fiat 20000
+;; cupom code with 20%
+
+;; Return all cars that are in clients budget considering cupom code
 
 (defn isCodeValid
   [code]
-  (println "todo isCodeValid"))
+  (defstruct coupon :Name :Discount) ;; struct definition
+  (def validCoupon (struct coupon "20%" 0.8)) ;; create struct object using definition
+  (if (= (:Name validCoupon) code)
+    true
+    false
+  ))
 
 (defn getCarPrices
   [budget code]
-  (def cars {"bmw" 6000, "ferrari" 100000, "fiat" 20000}))
+  (def cars {"bmw" 60000, "ferrari" 100000, "fiat" 20000})
+  (if (isCodeValid code)
+    (do
+      (println "The coupon code is valid")
+      (def discount (:Discount validCoupon))
+      (doseq [car cars]
+        (def carType (first car))
+        (def price (last car))
+        (def priceDiscount (* price discount))
+        (if (<= priceDiscount budget)
+          (println "the car" carType "costs" priceDiscount)
+        )
+      )
+    )
+
+    (do
+      (println "The coupon code is invalid")
+      (doseq [car cars]
+        (def carType (first car))
+        (def price (last car))
+        (if (<= price budget)
+          (println "the car" carType "costs" priceDiscount)
+        )
+      )
+    )
+  )
+  (println))
+
+(getCarPrices 50000 "20%")
+(getCarPrices 50000 "20asdad")
